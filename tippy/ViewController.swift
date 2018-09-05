d79606a1fb60deba15dffdf60102caa8655aeefd
vihnguyen <nguye29v@mtholyoke.edu>
2018-09-04 20:13:00 -0400

    Initial Commit

---
+++ tippy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tippy
+//
+//  Created by Mac Os on 9/4/18.
+//  Copyright © 2018 Vi Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
